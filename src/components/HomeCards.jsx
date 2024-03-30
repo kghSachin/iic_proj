d8e2d24b31eda6f4d310c4acc82d2e4896265354
sachin <118202383+kghSachin@users.noreply.github.com>
2024-03-30 09:17:29 +0545

    final proj

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -1,31 +1,29 @@
-import { Link } from 'react-router-dom';
-import Card from './Card';
+import { Link } from "react-router-dom";
+import Card from "./Card";
 
 const HomeCards = () => {
   return (
-    <section className='py-4'>
-      <div className='container-xl lg:container m-auto'>
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg'>
+    <section className="py-4">
+      <div className="container-xl lg:container m-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
           <Card>
-            <h2 className='text-2xl font-bold'>For Developers</h2>
-            <p className='mt-2 mb-4'>
-              Browse our React jobs and start your career today
-            </p>
+            <h2 className="text-2xl font-bold">For Developers</h2>
+            <p className="mt-2 mb-4">Browse jobs and start your career today</p>
             <Link
-              to='/jobs'
-              className='inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700'
+              to="/jobs"
+              className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
             >
               Browse Jobs
             </Link>
           </Card>
-          <Card bg='bg-indigo-100'>
-            <h2 className='text-2xl font-bold'>For Employers</h2>
-            <p className='mt-2 mb-4'>
-              List your job to find the perfect developer for the role
+          <Card bg="bg-indigo-100">
+            <h2 className="text-2xl font-bold">For Employers</h2>
+            <p className="mt-2 mb-4">
+              List your job to find the perfect person for the role
             </p>
             <Link
-              to='/add-job'
-              className='inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600'
+              to="/add-job"
+              className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
             >
               Add Job
             </Link>
